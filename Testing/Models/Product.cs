22e53e504b9aad0155b3f1e50adf3b8f3a931353
Jason Moore <moorejbm@gmail.com>
2021-03-03 11:18:18 -0600

    update

---
+++ Testing/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+
+            public int ProductID { get; set; }
+            public int NameID { get; set; }
+            public int PriceID { get; set; }
+            public int CategoryID { get; set; }
+            public int OnSaleID { get; set; }
+            public int StockLevelID { get; set; }
+    }
+
+    }
+
